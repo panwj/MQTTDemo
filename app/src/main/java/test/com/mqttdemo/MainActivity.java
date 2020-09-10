ef8435d88773fcc29b36550bf9bb732ca3a21776
panwj <18317713198@163.com>
2020-09-10 09:26:18 +0800

    add code

--- app/src/main/java/test/com/mqttdemo/MainActivity.java
+++ app/src/main/java/test/com/mqttdemo/MainActivity.java
@@ -3,14 +3,75 @@ package test.com.mqttdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TOPIC = "sub1";
+    private TextView mCreateTv, mSubTv, mPublishTv;
+    private EditText mSubEt, mPublishEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MqttManager.release();
+    }
+
+    private void init() {
+        mCreateTv = findViewById(R.id.tv_create);
+        mPublishTv = findViewById(R.id.tv_publish);
+        mSubTv = findViewById(R.id.tv_sub);
+
+        mSubEt = findViewById(R.id.et_sub);
+        mPublishEt = findViewById(R.id.et_publish);
+
+        mCreateTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (MqttManager.isCreateClient()) {
+                    if (!MqttManager.isConnected()) MqttManager.getInstance(getApplicationContext()).doConnect();
+                } else {
+                    String serverUri = "tcp://broker.emqx.io:1883";
+                    String clientId = "aa" + System.currentTimeMillis();
+                    MqttManager.getInstance(getApplicationContext()).createConnect(serverUri, clientId);
+                }
+            }
+        });
+
+        mPublishTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String publishMsg = mPublishEt.getText().toString();
+                MqttMessage mqttMessage = new MqttMessage();
+                mqttMessage.setPayload(publishMsg.getBytes());
+
+                String topic = mSubEt.getText().toString();
+                if (TextUtils.isEmpty(topic)) topic = TOPIC;
+                Log.d("MqttManager", "click msg : " + topic + "   " + publishMsg);
+                MqttManager.getInstance(getApplicationContext()).publishMessage(topic, mqttMessage);
+            }
+        });
+
+        mSubTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String subTopic = mSubEt.getText().toString();
+                if (TextUtils.isEmpty(subTopic)) subTopic = TOPIC;
+                Log.d("MqttManager", "click sub : " + subTopic);
+            }
+        });
     }
 }
---
+++ app/src/main/java/test/com/mqttdemo/MqttManager.java
@@ -0,0 +1,174 @@
+package test.com.mqttdemo;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MqttManager {
+
+    private static final String TAG = "MqttManager";
+    private static volatile MqttManager mInstance;
+    private static MqttAndroidClient mMqttAndroidClient;
+    private Context mContext;
+
+    public static MqttManager getInstance(Context context) {
+        if (mInstance == null) {
+            synchronized (MqttManager.class) {
+                if (mInstance == null) {
+                    mInstance = new MqttManager(context);
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    private MqttManager(Context context) {
+        mContext = context;
+    }
+
+    /**
+     * 释放单例, 及其所引用的资源
+     */
+    public static void release() {
+        try {
+            if (mInstance != null) {
+                disConnect();
+                mInstance = null;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "release : " + e.toString());
+        }
+    }
+
+    public void createConnect(String brokerUrl, String clientId) {
+        Log.d(TAG, "createConnect()...");
+        mMqttAndroidClient = new MqttAndroidClient(mContext, brokerUrl, clientId);
+        mMqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                Log.e(TAG, "connectComplete() ----> " + reconnect + "    " + serverURI);
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.e(TAG, "connectionLost() : " + (cause != null ? cause.toString() : null));
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.e(TAG, "messageArrived() : " + topic + "   message = " + message);
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.e(TAG, "deliveryComplete() : " + token);
+            }
+        });
+        doConnect();
+    }
+
+    /**
+     * 建立连接
+     */
+    public void doConnect() {
+        Log.d(TAG, "doConnect()  mMqttAndroidClient : " + mMqttAndroidClient);
+        if (mMqttAndroidClient != null) {
+            try {
+                MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+                mqttConnectOptions.setAutomaticReconnect(true);
+                //是否会建立持久性的回话，（应用场景接收离线消息）
+                mqttConnectOptions.setCleanSession(false);
+                //设置超时时间
+//                mqttConnectOptions.setConnectionTimeout(20);
+                //设置会话心跳时间
+//                mqttConnectOptions.setKeepAliveInterval(20);
+                //设置遗言消息
+//                mqttConnectOptions.setWill("home/rome/light1","test".getBytes(),1,true);
+
+                mMqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.e(TAG, "doConnect() ---> onSuccess()");
+                        DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                        disconnectedBufferOptions.setBufferEnabled(true);
+                        disconnectedBufferOptions.setBufferSize(100);
+                        disconnectedBufferOptions.setPersistBuffer(false);
+                        disconnectedBufferOptions.setDeleteOldestMessages(false);
+                        mMqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.e(TAG, "doConnect() ---> onFailure() : " + (exception != null ? exception.toString() : null));
+                    }
+                });
+            } catch (MqttException e){
+                e.printStackTrace();
+                Log.e(TAG, "doConnect()  exception : " + e.toString());
+            }
+        }
+    }
+
+    public void publishMessage(String publishTopic, MqttMessage mqttMessage) {
+        if (mMqttAndroidClient != null) {
+            try {
+                mMqttAndroidClient.publish(publishTopic, mqttMessage);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e(TAG, "publishMessage() exception : " + e.toString());
+            }
+        }
+    }
+
+    public void subscribeToTopic(String subscriptionTopic, int qos) {
+        if (mMqttAndroidClient != null) {
+            try {
+                mMqttAndroidClient.subscribe(subscriptionTopic, qos, null, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.e(TAG, "subscribeToTopic() ---> onSuccess()");
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.e(TAG, "subscribeToTopic() ---> onFailure() : " + (exception != null ? exception.toString() : null));
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "subscribeToTopic()  exception : " + e.toString());
+            }
+        }
+    }
+
+    public static void disConnect() {
+        try {
+            if (mMqttAndroidClient != null && mMqttAndroidClient.isConnected()) {
+                mMqttAndroidClient.disconnect();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "disConnect()  exception : " + e.toString());
+        }
+    }
+
+    public static boolean isCreateClient() {
+        return mMqttAndroidClient != null;
+    }
+
+    /**
+     * 判断是否连接
+     */
+    public static boolean isConnected() {
+        return mMqttAndroidClient != null && mMqttAndroidClient.isConnected();
+    }
+}
